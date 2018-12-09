4c9d37367cc9b904a55b7821c497023551739c29
Kosmik <malpa.w.klapkach@gmail.com>
2018-12-09 15:19:14 +0100

    ROCKER REAKCJE SA SUPER!

---
+++ SpaceRoyale/Assets/Scripts/RocketReaction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketReaction : MonoBehaviour
+{
+
+    public GameObject rocketPrefab;
+
+    public CapsuleCollider2D collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        collider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<RocketController>() != null)
+        {
+            Destroy(other.gameObject);
+
+            if (other.CompareTag("Rocket"))
+                Destroy(gameObject.transform.parent.gameObject);
+        }
+    }
+
+
+}
