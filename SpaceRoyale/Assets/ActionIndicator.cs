ebfaeafe5e62542e275824bd0b546d738834cc4b
Kosmik <malpa.w.klapkach@gmail.com>
2018-12-09 06:26:18 +0100

    UI wyswietla nastepna i current akcje

---
+++ SpaceRoyale/Assets/ActionIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionIndicator : MonoBehaviour {
+
+
+    public ActionController actionController;
+
+    public Sprite[] actionImages;
+
+    public Image[] nextActions;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        actionController = GameObject.FindGameObjectWithTag("Player").GetComponent<ActionController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		for(int i = 0; i < nextActions.Length; i++)
+        {
+            nextActions[i].sprite = actionImages[(int)actionController.NextActions[i]];
+        }
+	}
+}
