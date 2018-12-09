ccbff4e51f3d55f800cc7383b2014a6a3abdf749
Potatots <inny007@interia.pl>
2018-12-09 04:06:19 +0100

    SpawnerFix

--- SpaceRoyale/Assets/Scripts/Spawner.cs
+++ SpaceRoyale/Assets/Scripts/Spawner.cs
@@ -6,10 +6,15 @@ public class Spawner : MonoBehaviour
 {
     public int NumberOfObjects;
     public GameObject[] ListOfObjects;
+    public List<Vector3> SpawnedVectors;
+    public int MinimumDistance;
+    public int ScreenOffset;
 
     void Start()
     {
         SpawnEnemies();
+        SpawnedVectors = new List<Vector3>();
+        SpawnedVectors.Add(Vector3.zero);
     }
 
     public void SpawnEnemies()
@@ -17,9 +22,28 @@ public class Spawner : MonoBehaviour
         for(int i=0;i<NumberOfObjects;i++)
         {
             GameObject obj = ListOfObjects[Random.Range(0, ListOfObjects.Length - 1)];
+            Vector3 newVector = Vector3.zero;
+            do
+            {
+                newVector = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(ScreenOffset, Screen.width - ScreenOffset), Random.Range(ScreenOffset, Screen.height - ScreenOffset), Camera.main.farClipPlane / 2));
+            } while (!IsFarEnough(newVector));
 
-            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width-200), Random.Range(0, Screen.height-200), Camera.main.farClipPlane / 2));
-            Instantiate(obj, pos, Quaternion.identity);
+            Debug.Log(Screen.width);
+            Debug.Log(Screen.height);
+
+            SpawnedVectors.Add(newVector);
+
+            Instantiate(obj, newVector, Quaternion.identity);
         }
+    }
+
+    private bool IsFarEnough(Vector3 v)
+    {
+        foreach(Vector3 vector in SpawnedVectors)
+        {
+            if (Vector3.Distance(vector, v) < MinimumDistance)
+                return false;
+        }
+        return true;
     }
 }
