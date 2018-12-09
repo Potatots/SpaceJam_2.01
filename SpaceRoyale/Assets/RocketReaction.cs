85071e928455b79c03299859e6b9a7f0927f00de
Kosmik <malpa.w.klapkach@gmail.com>
2018-12-09 14:28:18 +0100

    push na wszelki wypadek

---
+++ SpaceRoyale/Assets/RocketReaction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketReaction : MonoBehaviour {
+
+    public GameObject rocketPrefab;
+
+    public CapsuleCollider2D collider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        collider = GetComponent<CapsuleCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
