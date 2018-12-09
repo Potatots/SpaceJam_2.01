368f29162581e7242b9fb227d5c6922bf4d12814
Potatots <inny007@interia.pl>
2018-12-09 02:18:38 +0100

    MainScene and Spawner

---
+++ SpaceRoyale/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int NumberOfObjects;
+    public GameObject[] ListOfObjects;
+
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    public void SpawnEnemies()
+    {
+        for(int i=0;i<NumberOfObjects;i++)
+        {
+            GameObject obj = ListOfObjects[Random.Range(0, ListOfObjects.Length - 1)];
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width-200), Random.Range(0, Screen.height-200), Camera.main.farClipPlane / 2));
+            Instantiate(obj, pos, Quaternion.identity);
+        }
+    }
+}
