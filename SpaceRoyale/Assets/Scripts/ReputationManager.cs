1e6d62c4dc399a39622a3f85a105d53604638ce8
Kosmik <malpa.w.klapkach@gmail.com>
2018-12-09 07:16:54 +0100

    początki systemu reputacji

---
+++ SpaceRoyale/Assets/Scripts/ReputationManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReputationManager : MonoBehaviour {
+
+    public int reputation;
+    public int maxReputation;
+
+    public int revolutionCount;
+    public int festivalCount;
+
+    // Use this for initialization
+    void Awake()
+    {
+        maxReputation = 10000;
+        revolutionCount = 0;
+        festivalCount = 0;
+        reputation = 5000;
+    }
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+}
