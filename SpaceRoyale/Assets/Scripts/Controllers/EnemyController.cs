65d1d52031cd7575b8d1ff658feeb00a6b4c0325
Potatots <inny007@interia.pl>
2018-12-09 09:43:06 +0100

    Dodany przeciwnik bydlak śmierdziel

---
+++ SpaceRoyale/Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject[] AllNpcs;
+    Transform SelectedNpc;
+
+    public float RotationSpeed;
+    public float MovementSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        AllNpcs = GameObject.FindGameObjectsWithTag("Npc");
+
+        SelectedNpc = AllNpcs[Random.Range(0, AllNpcs.Length)].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToNpc();
+    }
+
+    private void MoveToNpc()
+    {
+        Vector2 direction = SelectedNpc.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        rotation = Quaternion.Slerp(transform.rotation, rotation, RotationSpeed * Time.deltaTime);
+
+        transform.rotation = rotation;
+
+        transform.position = Vector2.MoveTowards(transform.position, SelectedNpc.position, MovementSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SelectedNpc = AllNpcs[Random.Range(0, AllNpcs.Length)].transform;
+    }
+}
