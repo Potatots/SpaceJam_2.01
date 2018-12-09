fc6c5e8a5976b26b452602387c5b9d51bb7c0400
Potatots <inny007@interia.pl>
2018-12-09 05:28:48 +0100

    Poprawki

--- SpaceRoyale/Assets/Scripts/Controllers/RocketController.cs
+++ SpaceRoyale/Assets/Scripts/Controllers/RocketController.cs
@@ -4,6 +4,11 @@ public class RocketController : MonoBehaviour {
 
     public float Speed;
 
+    public int minX = -55;
+    public int maxX = 55;
+    public int minY = -34;
+    public int maxY = 34;
+
 	// Use this for initialization
 	void Start () {
         Speed = 20;
@@ -12,6 +17,11 @@ public class RocketController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.up * Time.deltaTime * Speed);
+        if (transform.position.x < minX)// || transform.position.x > maxX)// || transform.position.y < minY || transform.position.y > maxY)
+        {
+            Debug.Log(transform.position.x);
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
--- SpaceRoyale/Assets/Scripts/Spawner.cs
+++ SpaceRoyale/Assets/Scripts/Spawner.cs
@@ -24,9 +24,15 @@ public class Spawner : MonoBehaviour
         {
             GameObject obj = ListOfObjects[Random.Range(0, ListOfObjects.Length - 1)];
             Vector3 newVector = Vector3.zero;
+            int counter = 0;
             do
             {
+                counter++;
                 newVector = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(HorizontalOffset, Screen.width - HorizontalOffset), Random.Range(VerticalOffset, Screen.height - VerticalOffset), Camera.main.farClipPlane / 2));
+
+                if (counter % 5 == 0 && MinimumDistance > 0)
+                    MinimumDistance--;
+
             } while (!IsFarEnough(newVector));
 
             SpawnedVectors.Add(newVector);
