a8e62f46d69228c7fbb1c0f4678881b2dd13eed4
Kosmik <malpa.w.klapkach@gmail.com>
2018-12-09 05:40:50 +0100

    Configurations rocket reactions

---
+++ SpaceRoyale/Assets/Scripts/ConfigurationScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "My Assets/ConfigurationObject")]
+
+public class ConfigurationScript : ScriptableObject
+{
+    public static int moneySatisfactionValue = 20;
+    public static int heartSatisfactionValue = 50;
+    public static int peaceSatisfactionValue = 100;
+    public static int rainbowSatisfactionValue = 200;
+}
